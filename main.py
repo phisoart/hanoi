3785249336cc8b0adf6f0cc9be9885ffa7c0096a
royjang12 <royjang12@gmail.com>
2022-10-18 14:01:27 +0900

    start

---
+++ main.py
@@ -0,0 +1,2 @@
+def test():    return Truedef hanoi(_height, _algorithm):    make input...    print(input)    output, comp_n = algorithm (input)    print(output)    return comp_nif __name__ == '__main__':    height = 10    if ~test(Algorithm ###):        print ('test fail')    comp_n = hanoi(height, algorithm)    print(height, comp_n)
+
